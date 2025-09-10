defa3643f6f8b38cc7611b95d9f67abfc1406420
Phelyp Munuera <mphelyp@gmail.com>
2025-09-10 08:30:50 -0300

    buttonBig ok

--- src/Componets/ButtonBig/index.tsx
+++ src/Componets/ButtonBig/index.tsx
@@ -1,7 +1,7 @@
 import { styles } from './styles';
 import { TouchableOpacity, Text } from 'react-native';
 
-export function ButtonBig({ value }) {
+export function ButtonBig({ value }: { value: string } ) {
   return (
     <TouchableOpacity style={styles.buttonBig}>
       <Text style={styles.textButton}>{value}</Text>
--- src/Componets/ButtonBig/styles.ts
+++ src/Componets/ButtonBig/styles.ts
@@ -6,18 +6,15 @@ export const styles = StyleSheet.create({
     height: 50,
     display: 'flex',
     backgroundColor: 'rgba(209, 213, 214, 1)',
-
     position: 'absolute',
     zIndex: 11,
-
     alignSelf: 'center',
-    marginTop: 650,
-
+    marginTop: 730,
     borderRadius: 50,
-
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   textButton: {
     color: 'rgba(19, 38, 47, 1)',
     fontSize: 24,
